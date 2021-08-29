43817fd596923c9151c9162db9ed7601d2b2d272
clearsky77 <clearsky3264@gmail.com>
2021-08-29 12:43:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/uicontrolpractice_20210829_drill/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uicontrolpractice_20210829_drill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
