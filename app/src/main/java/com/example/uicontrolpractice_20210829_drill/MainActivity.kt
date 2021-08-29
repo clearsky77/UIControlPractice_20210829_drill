afa484860cf51951b48d3c70bcc9691623cf8dfe
clearsky77 <clearsky3264@gmail.com>
2021-08-29 13:32:23 +0900

    UI 작업

--- app/src/main/java/com/example/uicontrolpractice_20210829_drill/MainActivity.kt
+++ app/src/main/java/com/example/uicontrolpractice_20210829_drill/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.uicontrolpractice_20210829_drill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        로그인 버튼을 누르면 아이디와 비밀번호를 받아온다
+        loginBtn.setOnClickListener {
+            var id = idText.text.toString()
+        }
+
+
+
     }
 }
