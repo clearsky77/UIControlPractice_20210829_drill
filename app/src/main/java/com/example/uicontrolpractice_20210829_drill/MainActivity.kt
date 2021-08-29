a682b62020ff8201e1ed24555817e02b4bd4e477
clearsky77 <clearsky3264@gmail.com>
2021-08-29 13:39:10 +0900

    아이디/비번 확인(if) - 둘 다 맞아야함( == / && )

--- app/src/main/java/com/example/uicontrolpractice_20210829_drill/MainActivity.kt
+++ app/src/main/java/com/example/uicontrolpractice_20210829_drill/MainActivity.kt
@@ -11,7 +11,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 //        로그인 버튼을 누르면 아이디와 비밀번호를 받아온다
         loginBtn.setOnClickListener {
-            var id = idText.text.toString()
+            var inputId = idEdt.text.toString()
+            var inputPw = pwEdt.text.toString()
+
+            if(inputId == "admin" && inputPw == "qwer"){
+                Toast.makeText(this, "관리자님 반갑습니다.", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "로그인이 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+
+
+
+
+
+
         }
 
 
